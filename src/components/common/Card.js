3b097aa0394c1bde423b8e7c4d63f0f5475005bc
taggaddaaaa <scaizerg@student.42.fr>
2017-09-27 08:01:43 +0200

    implement redux on app Add common components

---
+++ src/components/common/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+//create the component
+const Card = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
+};
+
+
+// Make the component available to other parts of the app
+export { Card };
